815245de801d662aedfa7f4aadd3e0352fd4ce44
Hazyapple <laelbymh@gmail.com>
2021-12-24 19:31:51 +0200

    Add collectible items, fix player movement

---
+++ Assets/Assets/Scripts/CollectibleItem.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CollectibleItem : MonoBehaviour
+{
+    private int Mayapples = 0;
+
+    [SerializeField] private Text MayapplesText;
+    private List<int> colletedIds = new List<int>();
+
+        private void OnTriggerEnter2D(Collider2D collision)
+        {
+            if (collision.gameObject.CompareTag("Mayapple"))
+            {   
+                if (!colletedIds.Contains(collision.gameObject.GetInstanceID()))
+                {
+                    Destroy(collision.gameObject);
+                    Mayapples++;
+                    colletedIds.Add(collision.gameObject.GetInstanceID());
+                    Debug.Log("Mayapples: " + Mayapples);
+                    MayapplesText.text = "Mayapples: " + Mayapples;
+                }
+                
+            }
+        }
+
+    }
+
---
+++ Assets/Assets/Scripts/Movement.cs
@@ -0,0 +1,80 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public float moveSpeed = 10f;
+    [SerializeField] private bool isGrounded = false;
+    private Animator animator;
+    private SpriteRenderer sprite;
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        sprite = GetComponent<SpriteRenderer>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Ground"))
+        {
+            isGrounded = true;
+            animator.SetBool("jump", false);
+            animator.SetFloat("speed", 0);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Ground"))
+            isGrounded = false;
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+
+        {
+            transform.Translate(new Vector3(-2, 0, 0) * Time.deltaTime);
+            sprite.flipX = true;
+        }
+
+
+
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Translate(new Vector3(2, 0, 0) * Time.deltaTime);
+            sprite.flipX = false;
+        }
+           
+ 
+
+        if (Input.GetButtonDown("Jump") && isGrounded == true)
+        {
+            GetComponent<Rigidbody2D>().AddForce(new Vector2(0f, 5f), ForceMode2D.Impulse);
+            animator.SetBool("jump", true);
+            animator.SetFloat("speed", 1);
+        }
+
+        //idle
+        if (isGrounded && (Input.GetKeyUp(KeyCode.LeftArrow) || Input.GetKeyUp(KeyCode.RightArrow)))
+        {
+            animator.SetFloat("speed", 0);
+        }
+
+        if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.RightArrow))
+        {
+            animator.SetFloat("speed", 1);
+        }
+        
+    }
+}
+
