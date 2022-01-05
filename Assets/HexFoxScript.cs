f35fd89dcc4eec99c517c26069192831e82d657c
Hazyapple <laelbymh@gmail.com>
2022-01-05 10:56:44 +0200

    HexbagPlant with working code for running and ins. hexbag collectible item

---
+++ Assets/HexFoxScript.cs
@@ -0,0 +1,103 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HexFoxScript : MonoBehaviour
+{
+    //agro away from player
+    [SerializeField] Transform player;
+    [SerializeField] float agroRange;
+    [SerializeField] float moveSpeed;
+
+
+    
+    public GameObject hexbag;    //prefab hexbag collectible item
+
+    private Vector3 position;
+
+    private SpriteRenderer sprite;
+
+    private Animator animator;
+
+    Rigidbody2D rb2d;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+        sprite = GetComponent<SpriteRenderer>();
+        animator = GetComponent<Animator>();
+    }
+
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+
+            this.position = transform.position;
+
+            createHexBag();
+
+            GetComponent<Renderer>().enabled = false;
+
+            destroySelf();
+        }
+    }
+
+    void createHexBag()
+    {
+        Instantiate(hexbag, new Vector2(this.position.x, transform.position.y), Quaternion.identity);
+    }
+
+    void destroySelf()
+    {
+        Destroy(this.gameObject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        float distToPlayer = Vector2.Distance(transform.position, player.position);
+
+        if (distToPlayer < agroRange)
+        {
+            //code to run from player
+            RunFromPlayer();
+            animator.SetBool("run", true);
+            animator.SetBool("idle", false);
+
+        }
+
+        else
+
+        {
+            //code to stop stop running from player
+            StopRunningFromPlayer();
+            animator.SetBool("run", false);
+            animator.SetBool("idle", true);
+        }
+
+
+        void RunFromPlayer()
+        {
+            if (transform.position.x < player.position.x)
+            {
+                rb2d.velocity = new Vector2(-moveSpeed, 0);
+                sprite.flipX = false;
+            }
+
+            else
+            {
+                rb2d.velocity = new Vector2(moveSpeed, 0);
+                sprite.flipX = true;
+            }
+        }
+
+        void StopRunningFromPlayer()
+        {
+            rb2d.velocity = new Vector2(0, 0);
+        }
+    }
+}
